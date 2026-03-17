31b2fe8cb43c8f6e7779d0e430586454c668310b
Everton Sanches <74841165+EvertonSanches@users.noreply.github.com>
2026-03-17 19:50:02 -0300

    Create aula8.py

---
+++ aula8.py
@@ -0,0 +1,9 @@
+#from math import sqrt, floor
+
+#num = int(input("Digite um numero: "))
+#raiz = sqrt(num)
+#print(f"A raiz quadrada de {num} é {floor(raiz):.2f}")
+
+import emoji
+
+print(f"Olá mundo 😎")
