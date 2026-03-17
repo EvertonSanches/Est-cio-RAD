a65520852de31d32428e7ee678ad6f8dc4fb983b
Everton Sanches <74841165+EvertonSanches@users.noreply.github.com>
2026-03-17 15:32:00 -0300

    Update exercicios2.py
    continuação dos exercícios.

--- exercicios2.py
+++ exercicios2.py
@@ -11,8 +11,23 @@
 #     print(f"Média: {media:.2f} - Reprovado")
 
 
-nome = input("Digite seu nome: ")
-idade = int(input("Digite sua idade: "))
-peso = float(input("Digite seu peso: "))
+# nome = input("Digite seu nome: ")
+# idade = int(input("Digite sua idade: "))
+# peso = float(input("Digite seu peso: "))
   
-print(nome, idade, peso)
+# print("Olá", nome, "Você tem", idade, "anos e pesa", peso, "kg.")
+
+# dia = int(input("Digite o dia do seu nascimento: "))
+# mes = (input("Digite o mês do seu nascimento: "))
+# ano = int(input("Digite o ano do seu nascimento: "))
+
+# print("Você nasceu em", dia, "de", mes, "de", ano)
+
+primeiro_numero = int(input("Digite o primeiro número: "))
+segundo_numero = int(input("Digite o segundo número: "))
+
+soma = primeiro_numero + segundo_numero 
+
+resultado = soma
+
+print("A soma entre", primeiro_numero, "e", segundo_numero, "é", resultado)
