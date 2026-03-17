22bedaee522a718b94c89b83e92a4c743a279519
Everton Sanches <74841165+EvertonSanches@users.noreply.github.com>
2026-03-17 17:01:16 -0300

    exercícios 5 a 10

--- exercicios2.py
+++ exercicios2.py
@@ -10,11 +10,10 @@
 # else:
 #     print(f"Média: {media:.2f} - Reprovado")
 
-
 # nome = input("Digite seu nome: ")
 # idade = int(input("Digite sua idade: "))
 # peso = float(input("Digite seu peso: "))
-  
+
 # print("Olá", nome, "Você tem", idade, "anos e pesa", peso, "kg.")
 
 # dia = int(input("Digite o dia do seu nascimento: "))
@@ -23,11 +22,72 @@
 
 # print("Você nasceu em", dia, "de", mes, "de", ano)
 
-primeiro_numero = int(input("Digite o primeiro número: "))
-segundo_numero = int(input("Digite o segundo número: "))
+#primeiro_numero = int(input("Digite o primeiro número: "))
+#segundo_numero = int(input("Digite o segundo número: "))
 
-soma = primeiro_numero + segundo_numero 
+#soma = primeiro_numero + segundo_numero
 
-resultado = soma
+#resultado = soma
 
-print("A soma entre", primeiro_numero, "e", segundo_numero, "é", resultado)
+#print("A soma entre", primeiro_numero, "e", segundo_numero, "é", resultado)
+
+#nome = input("Qual seu nome? ")
+#print("Prazer em te connhecer, {}!" .format(nome))
+
+#Exercício05:
+
+#valor = int(input("Digite um valor: "))
+#print("Analisando o valor {}, o seu antecessor é {}, e o seu sucessor é {}".format(valor, valor-1, valor+1))
+
+#Exercício06
+#num = int(input("Digite um numero: "))
+
+
+#print("o dobro de {} vale {}".format(num, (num*2)))
+#print("o triplo de {} vale {}. \nA raiz quadrada de {} é igual a {:.2f}.".format(num, (num*3), num, (num**(1/2))))
+
+#Exercicio07
+
+#n1 = float(input("Primeira Nota do Aluno: "))
+#n2 = float(input("Segunda Nota do Aluno: "))
+#n3 = float(input("Terceira Nota do Aluno: "))
+
+#media = (n1 + n2 + n3) / 3
+
+#print("A Média Final do aluno foi de: {:.2f}".format(media))
+
+#Exercício08:
+
+#medida = float(input("Digite o valor em metros: "))
+#cm = medida * 100
+#mm = medida * 1000
+
+#print(f'A medida de {medida} corresponde a {cm:.0f}cm e {mm:.0f}mm')
+
+#Exercício09
+
+#num = int(input("Digite um numero para ver sua tabuada: "))
+
+#tabuada = num * 1
+#print('-' * 18)
+#print(f"{num} x 1 = {tabuada}")
+#print(f"{num} x 2 = {tabuada*2}")
+#print(f"{num} x 3 = {tabuada*3}")
+#print(f"{num} x 4 = {tabuada*4}")
+#print(f"{num} x 5 = {tabuada*5}")
+#print(f"{num} x 6 = {tabuada*6}")
+#print(f"{num} x 7 = {tabuada*7}")
+#print(f"{num} x 8 = {tabuada*8}")
+#print(f"{num} x 9 = {tabuada*9}")
+#print(f"{num} x 10 = {tabuada*10}")
+#print('-' * 18)
+
+#Exercicio10:
+
+carteira = float(input("Quanto dinheiro você possue na carteira? R$"))
+
+dolar = 5.21
+
+conversor = carteira/dolar
+
+print(f"Com R${carteira} você pode comprar ${conversor:.2f} Dolar")
